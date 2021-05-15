fc9edefb0d414bde153941b5955fea972f34e65c
William Jung <79489182+williamjung97@users.noreply.github.com>
2021-05-15 01:23:37 -0400

    Add files via upload

---
+++ EmployeeSQL/table_schemata.sql
@@ -0,0 +1,70 @@
+-- Creating Tables
+CREATE TABLE "titles" (
+    "title_id" VARCHAR(10) NOT NULL,
+    "title" VARCHAR(30) NOT NULL,
+    CONSTRAINT "pk_titles" PRIMARY KEY (
+        "title_id"
+     )
+);
+
+CREATE TABLE "departments" (
+    "dept_no" VARCHAR(10) NOT NULL,
+    "dept_name" VARCHAR(30) NOT NULL,
+    CONSTRAINT "pk_departments" PRIMARY KEY (
+        "dept_no"
+     )
+);
+
+CREATE TABLE "employees" (
+    "emp_no" INTEGER NOT NULL,
+    "emp_title_id" VARCHAR(10) NOT NULL,
+    "birth_date" DATE NOT NULL,
+    "first_name" VARCHAR(20) NOT NULL,
+    "last_name" VARCHAR(20) NOT NULL,
+    "sex" VARCHAR(1) NOT NULL,
+    "hire_date" DATE NOT NULL,
+    CONSTRAINT "pk_employees" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "dept_manager" (
+    "dept_no" VARCHAR(10) NOT NULL,
+    "emp_no" INTEGER NOT NULL
+);
+
+CREATE TABLE "salaries" (
+    "emp_no" INTEGER NOT NULL,
+    "salary" INTEGER NOT NULL
+);
+
+CREATE TABLE "dept_emp" (
+    "emp_no" INTEGER NOT NULL,
+    "dept_no" VARCHAR(10) NOT NULL
+);
+
+-- Adding constraints 
+ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_title_id" FOREIGN KEY("emp_title_id")
+REFERENCES "titles" ("title_id");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
+
+SELECT * FROM departments;
+SELECT * FROM titles;
+SELECT * FROM employees;
+SELECT * FROM dept_emp;
+SELECT * FROM dept_manager;
+SELECT * FROM salaries;
